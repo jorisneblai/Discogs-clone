542285e9c5a76066b2ee807de0c2331bf7647f1e
Mboulgour <mboulgour@protonmail.com>
2022-11-19 15:26:27 +0100

    Show first records data on comp

--- src/Components/RecordsIndex/RecordsIndex.js
+++ src/Components/RecordsIndex/RecordsIndex.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDiscogs } from '../../Utils/useDiscogs';
 
 const RecordsIndex = (props) => {
+    const [records, setRecords] = useState();
     const data = useDiscogs('/users/jewice/collection/folders/0/releases', 1, 50);
+
+    useEffect(() => {
+        if (data) {
+            setRecords(data);
+        }
+    }, [data])
+
+
     return (
-        <>
-            RECORDS HERE and there
-        </>
+        <div>
+            {records && records.releases && records.releases.map(elm => elm.basic_information.title)}
+        </div>
     )
 }
 
