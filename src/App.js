a7e1fb20a10576dd63f20f02950730eb5e9c1d8b
joris neblai <joris.neblai@gmail.com>
2022-11-19 14:33:25 +0100

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import Navbar from './Components/Navbar/Navbar';
-=======
-import Navbar from "./Components/Navbar/Navbar";
->>>>>>> 82bffee7cb7047d72fd23ea2fbb381a99f24932d
 
 const App = () => {
   return (
