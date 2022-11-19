8574ced0698f227322a0452b7477ddef2c3cdb6e
joris neblai <joris.neblai@gmail.com>
2022-11-19 14:31:22 +0100

    creation of a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 
+import Navbar from './Components/Navbar/Navbar';
 
-function App() {
+const App = () => {
   return (
     <div>
-    
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HomeOutlined } from '@ant-design/icons';
+import './Navbar.css';
+
+const Navbar = (props) => {
+    return (
+        <div className="navbar">
+            <button className="navbar-button">
+                <HomeOutlined className="navbar-icon-home" />
+            </button>
+        </div>
+    )
+}
+
+export default Navbar;
