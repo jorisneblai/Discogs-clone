254e83d77236d17a0d09627036551db4d1cf9dd1
joris neblai <joris.neblai@gmail.com>
2022-11-19 15:57:46 +0100

    creation of the record component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { HomeOutlined } from '@ant-design/icons';
 // import VinylIcon from '../../Icons/vinyl.svg';
 import './Navbar.css';
--- src/Components/RecordsIndex/RecordsIndex.js
+++ src/Components/RecordsIndex/RecordsIndex.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDiscogs } from '../../Utils/useDiscogs';
-
+import Record from '../Record/Record';
+import './RecordsIndex.css';
 const RecordsIndex = (props) => {
     const [records, setRecords] = useState();
     const data = useDiscogs('/users/jewice/collection/folders/0/releases', 1, 10);
@@ -15,7 +16,7 @@ const RecordsIndex = (props) => {
     return (
         <div className='recordsIndexContainer'>
             {records && records.releases && records.releases.map(recordInfo => 
-
+                <Record key={recordInfo.id} info={recordInfo} />
             )}
         </div>
     )
