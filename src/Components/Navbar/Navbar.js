80f6d2439e5f1e0360fbedec0aab16df3d9e8709
joris neblai <joris.neblai@gmail.com>
2022-11-22 17:51:33 +0100

    adding vinyl icon on navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { HomeOutlined } from '@ant-design/icons';
-// import VinylIcon from '../../Icons/vinyl.svg';
+// import { HomeOutlined } from '@ant-design/icons';
+import Icon from '@ant-design/icons';
 import './Navbar.css';
+
+const VinylSvg = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="currentColor" className="bi bi-vinyl" viewBox="0 0 16 16">
+         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+         <path d="M8 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zM4 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0z"/>
+         <path d="M9 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+    </svg>
+);
+const VinylIcon = (props) => <Icon component={VinylSvg} />;
 
 const Navbar = (props) => {    
     return (
         <div className="navbar">
-            <HomeOutlined className="navbar-icon-home"/>
+            <button className="navbar-vinyl-btn">
+                <VinylIcon
+                    style={{
+                        fontSize: '16px',
+                        color: 'red',
+                    }}
+                />
+            </button>
         </div>
     )
 }
 
 export default Navbar;
+               
+            
