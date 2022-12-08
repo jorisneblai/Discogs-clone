61c87234cde4982172bded4a65efc15c3bf64ad3
Mboulgour <mboulgour@protonmail.com>
2022-12-08 12:50:32 +0100

    WIP

--- src/Components/RecordsIndex/RecordsIndex.js
+++ src/Components/RecordsIndex/RecordsIndex.js
@@ -1,27 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDiscogs } from '../../Utils/useDiscogs';
+import { Select } from 'antd';
 import Record from '../Record/Record';
 import './RecordsIndex.css';
 
 const RecordsIndex = (props) => {
-    const [{ data, isLoading, isError }, getRecords] = useDiscogs('/users/jewice/collection/folders/0/releases', 1, 10);
+    const [{ data, isLoading, isError }, getRecords] = useDiscogs();
+    const [sort, setSort] = useState("added")
 
     useEffect(() => {
-        getRecords();
+        getRecords(
+            {
+                endpoint: '/users/jewice/collection/folders/0/releases', 
+                page: 1, 
+                perPage: 10, 
+                sort: "added", 
+                sortOrder: "desc"
+            }
+        );
     }, [])
 
-    return (
-        <div className='recordsIndexContainer'>
-            {  isLoading ?
-                <div>Loading...</div>
-            :
-                data && data.releases && data.releases.map(recordInfo => 
-                    <Record 
-                        key={recordInfo.id} 
-                        info={recordInfo}
-                    />
-                )
+    const handleSort = (value) => {
+        getRecords(
+            {
+                endpoint: '/users/jewice/collection/folders/0/releases', 
+                page: 1, 
+                perPage: 10, 
+                sort: value, 
+                sortOrder: "desc"
             }
+        );
+    }
+
+    return (
+        <div className='recordsIndexWrapper'>
+            <Select
+                defaultValue={sort}
+                style={{ width: "max-content" }}
+                onChange={(value) => handleSort(value)}
+                options={[
+                    {
+                        value: 'added',
+                        label: 'Recently added',
+                    },
+                    {
+                        value: 'random',
+                        label: 'Random',
+                    },
+                ]}
+            />
+            <div className='recordsIndexContainer'>
+                {  isLoading ?
+                    <div>Loading...</div>
+                :
+                    data && data.releases && data.releases.map(recordInfo => 
+                        <Record 
+                            key={recordInfo.id} 
+                            info={recordInfo}
+                        />
+                    )
+                }
+            </div>
         </div>
     )
 }
--- src/Utils/useDiscogs.js
+++ src/Utils/useDiscogs.js
@@ -5,25 +5,28 @@ const API_URL = "https://api.discogs.com"
 const CONSUMER_KEY = "KHhlkeAcCQiYpBeUtOVp";
 const SECRET_KEY = "YKBpKkOZzbjDMWulsDbLlYYwFqcOGiQN";
 
-export const useDiscogs = (endPoint, page, perPage) => {
+export const useDiscogs = (endPoint, page, perPage, sort, sortOrder) => {
     const [data, setData] = useState();
-    const [url, setUrl] = useState(`${API_URL}/${endPoint}?page=${page}&perPage=${perPage}&key=${CONSUMER_KEY}&secret=${SECRET_KEY}`,);
+    const [url, setUrl] = useState(`${API_URL}/${endPoint}?page=${page}&perPage=${perPage}&sort=${sort}&sort_order=${sortOrder}&key=${CONSUMER_KEY}&secret=${SECRET_KEY}`);
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
   
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios(url).then((result) => {
-                    setData(result.data);
-                    setIsLoading(false)
-                })
-            } catch (error) {
-                setIsError(true);
-            }
-        };
-  
-        fetchData();
+        if (url) {
+            let finalUrl = `${API_URL}/${url.endPoint}?page=${url.page}&perPage=${url.perPage}&sort=${url.sort}&sort_order=${url.sortOrder}&key=${CONSUMER_KEY}&secret=${SECRET_KEY}`
+            const fetchData = async () => {
+                try {
+                    await axios(finalUrl).then((result) => {
+                        setData(result.data);
+                        setIsLoading(false)
+                    })
+                } catch (error) {
+                    setIsError(true);
+                }
+            };
+            
+            fetchData();
+        }
     }, [url]);
   
     return [{ data, isLoading, isError }, setUrl];
